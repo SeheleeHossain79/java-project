b07acef674ff3a227f175394f39b44777146ebba
SeheleeHossain79 <sohanaseheleehossain@gmail.com>
2024-11-30 14:36:46 +0600

    Add files via upload

---
+++ Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An online supershop website is a digital platform where users can browse, search, and purchase a wide variety of products, including groceries, household items, electronics, clothing, and more. It offers features like product categories, shopping carts, secure payment options, and home delivery services, enabling customers to shop conveniently from the comfort of their homes.</p>
+            <p>An online supershop website is a one-stop platform for shopping essentials and more. It offers seamless browsing, easy ordering, and convenient delivery, bringing a wide variety of products right to your doorstep.</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
